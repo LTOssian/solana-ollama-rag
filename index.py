b70a3b5091de49c7689e4d0cb083a7cbe2d01cb5
Louisan Tchitoula <t.louisan@outlook.fr>
2024-11-21 13:41:13 +0100

    SR_05 feat: add ollama embedding

--- index.py
+++ index.py
@@ -1,9 +1,17 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import OllamaEmbeddings
 from pypdf import PdfReader
 
+from app.prompt import prompt
 from app.s3 import s3_client
+from app.llm import llm
+from app.rag import RAGApplication
+
 BUCKET_NAME = "solana-documentation"
 FILE_NAME = "Solana_ A new architecture for a high performance blockchain.pdf"
+API_KEY = "api_key"
 
 # Download the file
 local_file_path = f"/tmp/{FILE_NAME}"  # Temporary storage for processing
@@ -26,3 +34,15 @@ print(f"Number of chunks created: {len(docs)}")
 # Display the split documents
 for idx, doc in enumerate(docs):
     print(f"Document Chunk {idx + 1}:\n{doc.page_content}\n")
+
+# Create OpenAIEmbeddings for documents and store in vectorstores
+embeddings = OllamaEmbeddings(model='llama3.2:1B')
+doc_vectors = [embeddings.embed_query(doc.page_content) for doc in docs]
+
+vector_store = FAISS.from_texts(
+    texts=[doc.page_content for doc in docs],
+    embedding=embeddings
+)
+
+retriever = vector_store.as_retriever(k=4)
+rag_chain = prompt | llm | StrOutputParser()
