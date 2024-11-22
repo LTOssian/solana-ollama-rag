952d88fca6bbf11fae050cbbd963fb11ebf58a90
Louisan Tchitoula <t.louisan@outlook.fr>
2024-11-22 14:24:36 +0100

    SR_15 fix: update langchain imports and add base prompt and llm app

--- app/llm.py
+++ app/llm.py
@@ -1,6 +1,20 @@
 from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain.llms.ollama import Ollama 
 
-llm = ChatOllama(
-    model="llama3.2:1b",
-    temperature=0,
-)
+llm = Ollama(model="llama3.2:1b")
+
+class BaseLLMApplication:
+
+    def __init__(self, llm_chain):
+        self.llm_chain = llm_chain
+
+    def run(self, question):
+        answer = self.llm_chain.invoke({"question": question})
+        return answer
+
+    @staticmethod
+    def initialize_base_llm(prompt, llm):
+        llm_chain = prompt | llm | StrOutputParser()
+        base_llm_application = BaseLLMApplication(llm_chain)
+        return base_llm_application
